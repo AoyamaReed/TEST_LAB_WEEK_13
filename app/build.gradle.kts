b70b1316fb9f40f04e3fbe5a2c032a752bdfc60b
Dominico <toni8ahia@gmail.com>
2025-12-03 16:30:36 +0700

    Commit No. 1 – add data binding

---
+++ app/build.gradle.kts
@@ -0,0 +1,81 @@
+import java.util.Properties
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+}
+
+fun getLocalProperty(key: String): String {
+    val localFile = rootProject.file("local.properties")
+    if (!localFile.exists()) return ""
+
+    val properties = Properties()
+    localFile.inputStream().use { input ->
+        properties.load(input)
+    }
+
+    return properties.getProperty(key) ?: ""
+}
+
+android {
+    namespace = "com.example.test_lab_week_13"
+    compileSdk = 36
+
+    buildFeatures {
+        buildConfig = true
+        dataBinding = true
+    }
+
+    defaultConfig {
+        applicationId = "com.example.test_lab_week_12"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        val apiKey = getLocalProperty("TMDB_API_KEY")
+        buildConfigField("String", "TMDB_API_KEY", "\"$apiKey\"")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.glide)
+    implementation(libs.retrofit)
+    implementation(libs.converter.moshi)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.15.0")
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
